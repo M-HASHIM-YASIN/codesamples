5065e78b68808f8f6ecc577cf592fe58fffebb03
Muhammad_Hashim_Yasin <hashimyasin96@gmail.com>
2023-01-04 23:33:05 +0500

    ** Arithmetic Operators **

---
+++ My_personal_Programs/C_Programs/Operators.c
@@ -0,0 +1,26 @@
+// There are 6 types o9f Operators are as Follows:
+
+//     1.  Arithmetic Operators
+//     2.  Relational Operators
+//     3.  Logical Operators
+//     4.  BitWise Operators
+//     5.  Assignment Operators
+//     6.  Misc Operators
+
+#include<stdio.h>
+
+int main()
+{
+    int a1 =3, b1=5;
+
+    printf("******* Arithmetic Operators******\n");
+    printf("The Sum of a1 and b1 is %d.\n",a1+b1);
+    printf("The Subtraction of a1 and b1 is %d.\n",a1-b1);
+    printf("The Multiplication of a1 and b1 is %d.\n",a1*b1);
+    printf("The Division of a1 and b1 is %d.\n",a1/b1);
+    printf("The Remainder of a1 and b1 is %d.\n",a1%b1);
+    printf("The Decrement of a1 and b1 is %d.\n",--b1);
+    printf("The Increment of a1 and b1 is %d.\n",++a1-b1);
+
+    return 0;
+}
