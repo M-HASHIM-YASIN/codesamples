ab6a7b282809300c1f6dc5ce6030d0a8b03fe5c5
Muhammad_Hashim_Yasin <hashimyasin96@gmail.com>
2023-01-05 15:20:51 +0500

    ** Pointer Updated **

--- My_personal_Programs/Functions_Arrays_Structures.c
+++ My_personal_Programs/Functions_Arrays_Structures.c
@@ -33,35 +33,42 @@ int main()
 
 
     // ****** Pointer ******
-    // printf("\n****** Pointers ******\n\n");
-    // printf("Pointer - (Call by refference)\n");
+    printf("\n****** Pointers ******\n\n");
+    printf("Pointer - (Call by refference)\n");
 
-    // int a = 125;
-    // printf("The Value of a is %d before pointer.\n",a);
+    int a = 125;
+    int b;
+    printf("The Value of a is %d before pointer.\n",a);
 
-    // int* ptr = NULL;
-    // if(ptr)
-    // {
-    //     printf("Pointer is not null");
-    // }
-    // ptr = &a; // &a means that address of a
-    // *ptr = 200;
-    // printf("The Value of a is %d after pointer.\n",a);
+    int* ptr1 = NULL;
+    int* ptr2 = NULL;
+    int c;
+    printf("Enter the value of c: ");
+    scanf("%d",&c);
+    if(ptr1)
+    {
+        printf("Pointer is not null");
+    }
+    ptr1 = &a; // &a means that address of a
+    ptr2 = &b;
+    *ptr1 = c;
+    *ptr2 = 700 + *ptr1;
+    printf("The Value of a is %d after pointer.\n",b);
 
 
     // ****** Arrays ******
-    printf("\n****** Arrays ******\n\n");
-    int ary[10] = {12,4,2,7,8,45,3,10,17,9};
+    // printf("\n****** Arrays ******\n\n");
+    // int ary[10] = {12,4,2,7,8,45,3,10,17,9};
 
     
-    for(int i=0; i<10; i++)  // Array 
-    {
-        for(int x=1; x<=10; x++)
-        {
-            printf(" %d * %d = %d\n",ary[i],x,ary[i]*x);
-        }
-        printf("\n");
-    }
+    // for(int i=0; i<10; i++)  // Array 
+    // {
+    //     for(int x=1; x<=10; x++)
+    //     {
+    //         printf(" %d * %d = %d\n",ary[i],x,ary[i]*x);
+    //     }
+    //     printf("\n");
+    // }
 
 
 
