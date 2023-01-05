81d19282dbae0c04ff14522aad55255270e1a4fd
Muhammad_Hashim_Yasin <hashimyasin96@gmail.com>
2023-01-05 14:34:01 +0500

    ** Pointers (Call by Refference) **

--- My_personal_Programs/Functions_Arrays_Structures.c
+++ My_personal_Programs/Functions_Arrays_Structures.c
@@ -24,10 +24,34 @@ float markavg(float x, float y, float z)
 }
 int main()
 {
-    printf("The Sum of 2 Numbers is %d.\n",marks(47,63));
-    printf("The Sum of 3 Numbers is %d.\n",mark(47,63,8));
-    printf("The Average of 2 Numbers is %f.\n",marksavg(47,63));
-    printf("The Average of 3 Numbers is %f.\n",markavg(47,63,8));
+    // Funstion Calling
+    printf("\n****** Functions Calling ******\n");
+    // printf("The Sum of 2 Numbers is %d.\n",marks(47,63));
+    // printf("The Sum of 3 Numbers is %d.\n",mark(47,63,8));
+    // printf("The Average of 2 Numbers is %f.\n",marksavg(47,63));
+    // printf("The Average of 3 Numbers is %f.\n",markavg(47,63,8));
 
+
+    // ****** Pointer ******
+    printf("\n****** Pointers ******\n\n");
+    printf("Pointer - (Call by refference)\n");
+
+    int a = 125;
+    printf("The Value of a is %d before pointer.\n",a);
+
+    int* ptr = NULL;
+    if(ptr)
+    {
+        printf("Pointer is not null");
+    }
+    ptr = &a; // &a means that address of a
+    *ptr = 200;
+    printf("The Value of a is %d after pointer.\n",a);
+
+
+
+
+
+    printf("\n");
     return 0;
 }
